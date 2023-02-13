ac6115262a98d918e020bf3cc7cf360abd8639ba
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-02-13 14:40:49 +0200

    Add advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find(req.query);
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte?|lte?)\b/g,
+      (match) => `$${match}`
+    );
+
+    const query = Tour.find(JSON.parse(queryString));
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
