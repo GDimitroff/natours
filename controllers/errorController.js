9ab724802e39a5b1de5da123afdf0b64aadaa5f8
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-02-21 10:55:42 +0200

    Handle mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,6 +2,7 @@ const AppError = require('../utils/appError');
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
+
   return new AppError(message, 400);
 };
 
@@ -9,6 +10,13 @@ const handleDuplicateFieldsDB = (err) => {
   const field = Object.keys(err.keyValue);
   const value = Object.values(err.keyValue);
   const message = `Duplicate ${field}: ${value}`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationError = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
 
   return new AppError(message, 400);
 };
@@ -53,6 +61,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err, name: err.name };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError') error = handleValidationError(error);
 
     sendErrorProd(error, res);
   }
