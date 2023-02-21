49eef771561f72c628ca268402216b3833dcd7a1
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-02-21 10:02:40 +0200

    Handle duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,14 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const field = Object.keys(err.keyValue);
+  const value = Object.values(err.keyValue);
+  const message = `Duplicate ${field}: ${value}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -43,9 +51,8 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err, name: err.name };
-    if (error.name === 'CastError') {
-      error = handleCastErrorDB(error);
-    }
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
