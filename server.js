bc16e9d93707be7948f2bf53b7483c8f1c0cd986
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2023-02-22 13:52:24 +0200

    Catch uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! 🔴 Shutting Down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -16,12 +23,19 @@ mongoose.connect(DB).then(() => {
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}... ✅`);
+  console.log(`App running on port ${port}... 👀`);
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! 🔴 Shutting Down...');
+  console.log(err.name, err.message);
+
+  server.close(() => process.exit(1));
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! 🔴 Shutting Down...');
+  console.log(err.name, err.message);
 
   server.close(() => process.exit(1));
 });
